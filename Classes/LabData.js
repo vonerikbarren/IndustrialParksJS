2c256369b965bf0ca8e3254662e6a8cb2c60dc7b
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2021-11-01 19:00:30 -0400

    adding a finished testable version of Lab Class

---
+++ Classes/LabData.js
@@ -0,0 +1,104 @@
+class LabData {
+  constructor(
+    C1_DateStart = String,
+    C2_DateEnd = String,
+    C3_TimeStart = Number,
+    C4_TimeEnd = Number,
+    C5_DataTitle = String,
+    C6_DataTags = Array,
+  ) {
+    this.C1_DateStart = C1_DateStart;
+    this.C2_DateEnd = C2_DateEnd;
+    this.C3_TimeStart = C3_TimeStart;
+    this.C4_TimeEnd = C4_TimeEnd;
+    this.C5_DataTitle = C5_DataTitle;
+    this.C6_DataTags = C6_DataTags;
+
+    // Storage + Log
+    this.Storage = [
+      {
+        DA0_DataID: 0,
+        DA1_DataLog_TimeStart: [],
+        DA2_DataLog_TimeFinish: [],
+        DA3_DataEntry: [],
+        DA4_DataSummaryOutput: [],
+      },
+
+      {
+        DB0_DataID: 0,
+        DB1_DataLog_TimeStart: [],
+        DB2_DataLog_TimeFinish: [],
+        DB3_DataEntry: [],
+        DB4_DataSummaryOutput: [],
+      },
+
+      {
+        DC0_DataID: 0,
+        DC1_DataLog_TimeStart: [],
+        DC2_DataLog_TimeFinish: [],
+        DC3_DataEntry: [],
+        DC4_DataSummaryOutput: [],
+      },
+
+      {
+        DD0_DataID: 0,
+        DD1_DataLog_TimeStart: [],
+        DD2_DataLog_TimeFinish: [],
+        DD3_DataEntry: [],
+        DD4_DataSummaryOutput: [],
+      },
+
+      {
+        DE0_DataID: 0,
+        DE1_DataLog_TimeStart: [],
+        DE2_DataLog_TimeFinish: [],
+        DE3_DataEntry: [],
+        DE4_DataSummaryOutput: [],
+      },
+    ];
+  }
+
+  fxA0_printLabData = (dataSection, dataAttribute) => {
+    console.log(this.Storage);
+  }
+
+  fxA1_addLabData = (
+    DataID,
+    TimeStart,
+    TimeFinish,
+    DataEntry,
+    SummaryOutput
+  ) => {
+    if (DataID === 0) {
+      this.Storage[0].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[0].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[0].DA3_DataEntry.push(DataEntry);
+      this.Storage[0].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else if (DataID === 1) {
+      this.Storage[1].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[1].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[1].DA3_DataEntry.push(DataEntry);
+      this.Storage[1].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else if (DataID === 2) {
+      this.Storage[2].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[2].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[2].DA3_DataEntry.push(DataEntry);
+      this.Storage[2].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else if (DataID === 3) {
+      this.Storage[3].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[3].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[3].DA3_DataEntry.push(DataEntry);
+      this.Storage[3].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else if (DataID === 4) {
+      this.Storage[4].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[4].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[4].DA3_DataEntry.push(DataEntry);
+      this.Storage[4].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else if (DataID === 5) {
+      this.Storage[5].DA1_DataLog_TimeStart.push(TimeStart);
+      this.Storage[5].DA2_DataLog_TimeFinish.push(TimeFinish);
+      this.Storage[5].DA3_DataEntry.push(DataEntry);
+      this.Storage[5].DA4_DataSummaryOutput.push(SummaryOutput);
+    } else { console.log(Error) }
+  }
+}
